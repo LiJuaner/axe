bc5993f392c7fe0c632378df8429d2971c40e00b
lijuan.li <lijuan.li@hand-china.com>
2024-04-07 16:52:19 +0800

    新建axe项目

---
+++ axe-web/src/test/java/com/hxy/lab/axe/AxeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxy.lab.axe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
