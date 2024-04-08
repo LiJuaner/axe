709bdf248788b1a1b223387099e4f405c4060c89
lijuan.li <lijuan.li@hand-china.com>
2024-04-08 15:49:42 +0800

    使用Mybatis-plus生成相关文件

---
+++ axe-model/src/main/java/com/hxy/lab/axe/model/entity/People.java
@@ -0,0 +1,94 @@
+package com.hxy.lab.axe.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.time.LocalDate;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author LiJuan.Li
+ * @since 2024-04-08
+ */
+@TableName("people")
+@Schema(name = "People", description = "")
+public class People implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Schema(description = "ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @Schema(description = "姓名")
+    @TableField("name")
+    private String name;
+
+    @Schema(description = "性别")
+    @TableField("sex")
+    private String sex;
+
+    @Schema(description = "年龄")
+    @TableField("birthday")
+    private LocalDate birthday;
+
+    @TableField("province")
+    private String province;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+            "id = " + id +
+            ", name = " + name +
+            ", sex = " + sex +
+            ", birthday = " + birthday +
+            ", province = " + province +
+        "}";
+    }
+}
---
+++ axe-service/src/main/java/com/hxy/lab/axe/dao/PeopleMapper.java
@@ -0,0 +1,16 @@
+package com.hxy.lab.axe.dao;
+
+import com.hxy.lab.axe.model.entity.People;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author LiJuan.Li
+ * @since 2024-04-08
+ */
+public interface PeopleMapper extends BaseMapper<People> {
+
+}
